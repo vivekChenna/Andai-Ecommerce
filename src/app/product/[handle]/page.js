94610a741d6fcefc1cf39ab36c79529676a2ca0b
vivekChenna <chennavivek96@gmail.com>
2024-12-19 11:02:56 +0530

    added carousel to single plugin page

--- src/app/product/[handle]/page.js
+++ src/app/product/[handle]/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import { Carousel } from "@/components/carousel";
 import { GridTileImage } from "@/components/grid/tile";
 // import type { Metadata } from 'next';
 // import { notFound } from 'next/navigation';
@@ -51,7 +52,6 @@ export default function ProductPage({ params }) {
     },
   });
 
-
   return (
     <>
       <div className="mx-auto max-w-screen-2xl px-4 text-black  bg-[#FFF8E3]">
@@ -62,21 +62,27 @@ export default function ProductPage({ params }) {
                 <div className="relative aspect-square h-full max-h-[550px] w-full overflow-hidden" />
               }
             >
-             <Gallery
-              image={data?.plugins[0]?.img_url || `/svg/product.svg`}
-              isLoading={loading}
-            />
+              <Gallery
+                image={data?.plugins[0]?.img_url || `/svg/product.svg`}
+                isLoading={loading}
+              />
             </Suspense>
           </div>
 
           <div className="basis-full lg:basis-2/6">
-            <ProductDescription product={data?.plugins[0]} isLoading={loading} />
+            <ProductDescription
+              product={data?.plugins[0]}
+              isLoading={loading}
+            />
           </div>
         </div>
-        <RelatedProducts
+        {/* <RelatedProducts
           id={data?.plugins[0]?.category_id}
           filterId={data?.plugins[0]?.id}
-        />
+        /> */}
+        <div className="mt-4">
+          <Carousel />
+        </div>
       </div>
       <Footer />
     </>
