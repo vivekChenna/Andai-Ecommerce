eb39291bf91081ebda8c44a13bb97fc83049be95
vivekChenna <chennavivek96@gmail.com>
2024-12-23 16:20:21 +0530

    added suspense in search

--- src/app/page.js
+++ src/app/page.js
@@ -5,14 +5,17 @@ import Hero from "@/components/Home/Hero";
 import Marketplace from "@/components/Home/Marketplace";
 import SearchFeature from "@/components/Home/SearchFeature";
 import Footer from "@/components/layout/footer";
-import Search from "@/components/layout/navbar/search";
+import Search, { SearchSkeleton } from "@/components/layout/navbar/search";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="bg-[#FFF8E3]">
       <Hero />
       <Features />
-      <SearchFeature />
+      <Suspense fallback={<SearchSkeleton />}>
+        <SearchFeature />
+      </Suspense>
       <Marketplace />
       <Footer />
     </main>
