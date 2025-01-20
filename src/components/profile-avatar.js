8cd94588f17dae8802485fef69304a61e22cd485
vivekChenna <chennavivek96@gmail.com>
2025-01-20 13:13:41 +0530

    changed font

--- src/components/profile-avatar.js
+++ src/components/profile-avatar.js
@@ -20,7 +20,7 @@ export function ProfileAvatar({ name, imageUrl }) {
   
 
   return (
-    <div className="flex h-8 w-8 items-center justify-center rounded-full andai:bg-black light:bg-black dark:bg-white andai:text-white light:text-white dark:text-black  text-primary-foreground">
+    <div className="flex h-8 w-8 items-center justify-center rounded-full andai:bg-black light:bg-black dark:bg-white andai:text-white light:text-white dark:text-black  text-primary-foreground font-semibold">
       {initial}
     </div>
   )
